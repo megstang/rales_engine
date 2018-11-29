d88f271806fb2f9fb6a6f1dc8e0e293f87ab5ba1
Meghan Stang <mstang15@gmail.com>
2018-11-29 09:38:50 -0700

    adds most_revenue for merchants by quantity

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    
+    render json: Merchant.most_revenue(params[:quantity].to_i)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def self.most_revenue(quantity)
+    select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price/100) as revenue')
+    .joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'})
+    .group(:id)
+    .order('revenue desc')
+    .limit(quantity)
+  end
 end
