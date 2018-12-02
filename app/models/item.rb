a4e8da5b32ccb8d4784340a117165f81e04b361d
Meghan Stang <mstang15@gmail.com>
2018-12-02 00:20:42 -0700

    adds most_items for items by quantity

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def self.most_revenue(quantity)
     select('items.*, sum(invoice_items.unit_price * invoice_items.quantity) as revenue')
@@ -9,4 +11,13 @@ class Item < ApplicationRecord
     .group(:id)
     .limit(quantity)
   end
+
+  def self.most_items(quantity)
+     select('items.*, sum(invoice_items.quantity) as item_quant')
+     .joins(:invoice_items, :transactions )
+     .group(:id)
+     .where(transactions: {result: 'success'})
+     .order('item_quant desc')
+     .limit(quantity)
+   end
 end
