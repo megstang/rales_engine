3162ddde8e67a8e1aaf30e8b14789b6d74f53ca1
Meghan Stang <mstang15@gmail.com>
2018-12-01 23:48:28 -0700

    adds most_revenue for items by quantity

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+
+  def self.most_revenue(quantity)
+    select('items.*, sum(invoice_items.unit_price * invoice_items.quantity) as revenue')
+    .joins(:invoice_items)
+    .joins('join transactions on invoice_items.invoice_id = transactions.invoice_id')
+    .group(:id)
+    .limit(quantity)
+  end
 end
